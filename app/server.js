d7ad6345a7b7895ea5044d3b1864c8e994da9867
Zachary Bergmann <zbbergma@gmail.com>
2019-10-17 02:56:16 -0400

    Add testing framework, eslint, and a basic app server

---
+++ app/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const PORT = process.env.PORT || 8000;
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, (err) => {
+  if (!err) {
+    process.stdout.write(`Server is running on port ${PORT}!\n`);
+  }
+});
+
+app.use(morgan('dev'));
---
+++ app/server.test.js
@@ -0,0 +1,7 @@
+const assert = require('chai').assert;
+
+describe('Server Application', () => {
+  it('should exist', () => {
+    assert.equal(1, 1);
+  });
+});
