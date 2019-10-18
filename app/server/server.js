ee27d6a0e4ac792dd95bc0250849ad19e24a9ea2
Zachary Bergmann <zbbergma@gmail.com>
2019-10-17 22:56:32 -0400

    Add html and javascript to get basic UI for application

--- app/server/server.js
+++ app/server/server.js
@@ -1,23 +1,44 @@
 require('dotenv').config();
 
-const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const ContactInfo = require('../models/ContactInfo');
 
 const PORT = process.env.PORT || 8000;
 const app = express();
-
-app.use(morgan('dev'));
-app.set('views', './app/views');
-app.set('view engine', 'pug');
-app.use(express.static(`${process.cwd()}/app/public`));
-
-app.get('/', (req, res) => {
-  res.render('index', { title: 'OCR Text Parser', message: 'Hello there!' });
-});
 
 app.listen(PORT, (err) => {
   if (!err) {
     process.stdout.write(`Server is running on port ${PORT}!\n`);
   }
+});
+
+// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static(`${process.cwd()}/app/public`));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+app.use(morgan('dev'));
+app.set('views', './app/views');
+app.set('view engine', 'pug');
+
+app.post('/parseOcrText', (req, res) => {
+  // go ahead and parse into a ContactInfo object
+  console.log(req.body);
+  const aContactInfo = new ContactInfo('Zachary B', '1231231231', 'tests@test.com');
+  res.statusCode = 200;
+  // res.setHeader('Content-Type', 'application/json');
+  // if valid
+    // sent valid response and data back in new form
+  // else
+    // send invalid response and error message encountered
+  res.send({ name: aContactInfo.getName(), emailAddress: aContactInfo.emailAddress, phoneNumber: aContactInfo.phoneNumber });
+});
+
+app.get('/', (req, res) => {
+  res.render('index', { title: 'OCR Text Parser', message: 'Hello there!', outputText: 'Testing output text here' });
 });
