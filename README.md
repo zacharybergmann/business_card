883535f7153a9a813b275e49dedb51618d0f949c
Zachary Bergmann <zbbergma@gmail.com>
2019-10-20 13:13:15 -0400

    Add comments to README.md, test and modify for curl usage to app server

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # business_card
 A business card application that reads OCR business contacts and outputs the desired extracted fields
+
+# To Run Application
+Node 10 LTS or later is required!
+```
+cd business_card
+npm i
+npm start
+```
+
+At this point, the dev server is running and the web page can be visited at localhost:8000
+
+Each business card OCR text can be pasted into the input field and outputs will be observed below
+
+Another way to use this application as a service would be to use the following command replacing your data with the demo data
+
+```
+curl -XPOST http://localhost:8008/parseOcrText -i -H "Content-Type: application/json" -d '{"inputText": "ASYMMETRIK LTD\nMike Smith\nSenior Software Engineer\n(410)555-1234\nmsmith@asymmetrik.com\n"}'
+```
--- app/server/server.js
+++ app/server/server.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-
 const businessCardParser = require('../controllers/businessCardParser');
 
 const PORT = process.env.PORT || 8000;
@@ -17,9 +16,9 @@ app.listen(PORT, (err) => {
 // app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(`${process.cwd()}/app/public`));
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 app.use(morgan('dev'));
@@ -27,9 +26,22 @@ app.set('views', './app/views');
 app.set('view engine', 'pug');
 
 app.post('/parseOcrText', (req, res) => {
-  const aContactInfo = businessCardParser.getContactInfo(req.body.inputText);
-  res.statusCode = 200;
-  res.send({ name: aContactInfo.getName(), emailAddress: aContactInfo.emailAddress, phoneNumber: aContactInfo.phoneNumber });
+  try {
+    if (Object.prototype.hasOwnProperty.call(req.body, 'inputText')) {
+      const aContactInfo = businessCardParser.getContactInfo(req.body.inputText);
+      res.statusCode = 200;
+      res.send({
+        name: aContactInfo.getName(),
+        emailAddress: aContactInfo.emailAddress,
+        phoneNumber: aContactInfo.phoneNumber,
+      });
+    } else {
+      throw Error('Invalid request format');
+    }
+  } catch (err) {
+    res.status = 400;
+    res.send({ error: err.message });
+  }
 });
 
 app.get('/', (req, res) => {
