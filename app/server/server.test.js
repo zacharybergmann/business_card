411399ced212095f45d33ac9a2daac0baa142072
Zachary Bergmann <zbbergma@gmail.com>
2019-10-20 20:17:21 -0400

    Continue work on server tests

--- app/server/server.test.js
+++ app/server/server.test.js
@@ -1,19 +1,23 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const server = require('./server');
+const serverImport = require('./server');
 
-const { assert } = chai;
-
+const { assert, expect } = chai;
 chai.use(chaiHttp);
+const server = chai.request(serverImport).keepOpen();
+
+after(() => {
+  server.close();
+});
 
 describe('Server Application', () => {
   describe('GET /', () => {
     it('it should GET the html web page', (done) => {
-      chai.request(server)
+      server
         .get('/')
         .end((err, res) => {
           assert(res.status, 200);
-          assert(res.body, 'test');
+          expect(res).to.be.html; // eslint-disable-line no-unused-expressions
           done();
         });
     });
@@ -21,7 +25,7 @@ describe('Server Application', () => {
   describe('POST /parseOcrText', () => {
     it('it should return a 400 status and an object with an error message if an inputText field is not passed', (done) => {
       const body = {};
-      chai.request(server)
+      server
         .post('/parseOcrText')
         .send(body)
         .end((err, res) => {
@@ -35,7 +39,7 @@ describe('Server Application', () => {
       const body = {
         inputText: 'ASYMMETRIK LTD\nMike Smith\nSenior Software Engineer\n(410)555-1234\nmsmith@asymmetrik.com',
       };
-      chai.request(server)
+      server
         .post('/parseOcrText')
         .send(body)
         .end((err, res) => {
@@ -53,7 +57,7 @@ describe('Server Application', () => {
       const body = {
         inputText: 'Foobar Technologies\nAnalytic Developer\nLisa Haung\n1234 Sentry Road\nColumbia, MD 12345\nPhone: 410-555-1234\nFax: 410-555-4321\nlisa.haung@foobartech.com',
       };
-      chai.request(server)
+      server
         .post('/parseOcrText')
         .send(body)
         .end((err, res) => {
@@ -71,7 +75,7 @@ describe('Server Application', () => {
       const body = {
         inputText: 'Arthur Wilson\nSoftware Engineer\nDecision & Security Technologies\nABC Technologies\n123 North 11th Street\nSuite 229\nArlington, VA 22209\nTel: +1 (703) 555-1259\nFax: +1 (703) 555-1200\nawilson@abctech.com',
       };
-      chai.request(server)
+      server
         .post('/parseOcrText')
         .send(body)
         .end((err, res) => {
