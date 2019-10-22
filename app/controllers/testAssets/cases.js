d968d8087c2ff349f888768813391a39c9bee8d2
Zachary Bergmann <zbbergma@gmail.com>
2019-10-22 02:10:23 -0400

    Add 2 more test cases

--- app/controllers/testAssets/cases.js
+++ app/controllers/testAssets/cases.js
@@ -23,6 +23,22 @@ const cases = [
       phone: '17035551259',
     },
   },
+  {
+    input: 'John Inc\nSoftware Engineer\nDecision & Security Technologies\nABC Technologies\n123 North 11th Street\nSuite 229\nArlington, VA 22209\nTel: +1 (703) 555-1259\nFax: +1 (703) 555-1200\nj.inc@abctech.com',
+    output: {
+      name: 'John Inc',
+      email: 'j.inc@abctech.com',
+      phone: '17035551259',
+    },
+  },
+  {
+    input: 'Kwali Swelta\nSoftware Engineer\nDecision & Security Technologies\nABC Technologies\n123 North 11th Street\nSuite 229\nArlington, VA 22209\nF: +1 (703) 555-1200\nT: +1 (703) 555-1259\nswelta1@abctech.com',
+    output: {
+      name: 'Kwali Swelta',
+      email: 'swelta1@abctech.com',
+      phone: '17035551259',
+    },
+  },
 ];
 
 module.exports = cases;
